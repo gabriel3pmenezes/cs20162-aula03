b902208a538fdf611b8c72c2a13ea02a66a381d2
gabriel3pmenezes <biellego@hotmail.com>
2016-09-28 15:38:39 -0300

    Add files via upload

--- MVNExe14/src/test/java/com/mycompany/mvnexe14/Teste14.java
+++ MVNExe14/src/test/java/com/mycompany/mvnexe14/Teste14.java
@@ -8,13 +8,18 @@ public class Teste14 {
 
 
     @Test(expected = IllegalArgumentException.class)
-    public void numMenorqueValido() {
+    public void numMenorqueValido1() {
         MVNExe14.LogaritmoNatural(-4,1);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void numMenorqueValido2() {
+        MVNExe14.Fatorial(-4);
     }
 
     @Test
     public void umNumeroArbitrarioCorreto() {
-        assertEquals(16.375, MVNExe14.LogaritmoNatural(3, 4));
+        assertEquals(16.375, MVNExe14.LogaritmoNatural(3, 4), 0.01);
     }
 
    
