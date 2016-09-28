a9e2f75c19e6f4fe86a70a24933f3c90fd279e79
gabriel3pmenezes <biellego@hotmail.com>
2016-09-28 15:37:51 -0300

    Add files via upload

--- MVNExe13/src/main/java/com/mycompany/mvnexe13/MVNExe13.java
+++ MVNExe13/src/main/java/com/mycompany/mvnexe13/MVNExe13.java
@@ -3,26 +3,17 @@ package com.mycompany.mvnexe13;
 
 import java.util.Scanner;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Gabriel
- */
 public class MVNExe13 {
 
-    public static float Fatorial(float n){
+    public static int Fatorial(int n){
         
         if (n < 1){
             throw new IllegalArgumentException("Número fora da faixa");
         }
         
-        float i = 2;
-        float f = 1;
+        int i = 2;
+        int f = 1;
         
         while(i <= n){
             f = f * i;
@@ -35,8 +26,8 @@ public class MVNExe13 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        float n;
-        float resultado;
+        int n;
+        int resultado;
         Scanner leitor = new Scanner(System.in);
         
         System.out.println("Insira o valor de n: ");
